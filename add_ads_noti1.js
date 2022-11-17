9dea2fda472789eccddd4ad5f4105bfded12a408
thanhluongtrungit <118371709+thanhluongtrungit@users.noreply.github.com>
2022-11-17 08:43:48 +0700

    Create add_ads_noti1.js

---
+++ add_ads_noti1.js
@@ -0,0 +1,10 @@
+    bodyNode = document.querySelector("body");
+    
+    adsNodeNoti = document.createElement("div");
+    adsNodeNoti.innerHTML = "<div id='M843947ScriptRootC1386782'></div>"
+    scriptNoti = document.createElement("script");
+  	scriptNoti.setAttribute('src','https://jsc.adskeeper.com/l/o/lovepumi.store.1386782.js');
+  	scriptNoti.setAttribute('async','async');
+    adsNodeNoti.append(scriptNoti)
+    
+    bodyNode.append(adsNodeNoti);
