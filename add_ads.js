ba27747c960d65cd0560d54d9ff890663203ade2
thanhluongtrungit <118371709+thanhluongtrungit@users.noreply.github.com>
2022-11-17 07:23:50 +0700

    Update add_ads.js

--- add_ads.js
+++ add_ads.js
@@ -12,15 +12,4 @@
 
     figureNodes[i].insertAdjacentElement('afterend', adsNode);
     
-    let titleNode = document.querySelector("div.post-title-container");
-    
-    
-    let adsNode1 = document.createElement("div");
-    adsNode1.innerHTML = "<div id='M843947ScriptRootC1385920'></div>"
-    let script1 = document.createElement("script");
-  	script1.setAttribute('src','https://jsc.adskeeper.com/l/o/lovepumi.store.1385920.js');
-  	script1.setAttribute('async','async');
-    adsNode1.append(script1)
-    
-    titleNode.insertAdjacentElement('afterend', adsNode1);
   }
