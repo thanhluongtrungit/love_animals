95904c07fa761e2168b19d350ba3f052d69660b9
thanhluongtrungit <118371709+thanhluongtrungit@users.noreply.github.com>
2022-11-22 20:06:28 +0700

    Create add_ads_under_article3.js

---
+++ add_ads_under_article3.js
@@ -0,0 +1,10 @@
+    postBodyNode = document.querySelector("div.post-footer");
+
+    adsNode2 = document.createElement("div");
+    adsNode2.innerHTML = "<div id='M843947ScriptRootC1385939'></div>"
+    script2 = document.createElement("script");
+  	script2.setAttribute('src','https://jsc.adskeeper.com/l/o/lovepumi.store.1385939.js');
+  	script2.setAttribute('async','async');
+    adsNode2.append(script2)
+    
+    postBodyNode.prepend(adsNode2);
